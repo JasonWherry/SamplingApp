ce581d20a3aaac2144b08b76c5c5ff9db7ab5b12
Jason Wherry <jw473415@ohio.edu>
2020-05-25 12:57:11 -0400

    Program now displays the four moments of the collection of samples: mean of means, standard deviation, skewness, & kurtosis.

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import pandas as pd
 from pandas import DataFrame
 import seaborn as sb
 import matplotlib.pyplot as plt
-from statistics import stdev
+from statistics import *
+import scipy.stats as stats
 
 
 """
@@ -43,7 +44,7 @@ def prompt_user():
 		col_name = 'Case ' + str(i+1)
 		df.insert(i, col_name, temp)
 
-	print(df, '\n') # display np.random.uniform numbers (dtype is float64) by sample
+	# print(df, '\n') # display np.random.uniform numbers (dtype is float64) by sample
 
 	return df
 
@@ -70,13 +71,21 @@ def find_stats(df):
 	if sample_num == 1:
 		# there is one sample & we need more than 1 value to calculate std_dev
 		# std_dev = round( df.iloc[0:, i].std(), 3) # old way to calculate std_dev for just 1 sample
+		mean_of_means = means[0]
 		std_dev = round( df.iloc[0:, i].mean().std(), 3)
+		skewness = round( df.iloc[0:, i].skew(), 3)
+		kurtosis = round( df.iloc[0:, i].kurtosis(), 3)
 
 	else:
 		# calculates the std. deviation of each sample's mean
+		numpy_means = np.array(means)
+		mean_of_means = round( numpy_means.mean(), 3)
 		std_dev = round( stdev(means), 3)
+		skewness = round( stats.skew(means), 3)
+		kurtosis = round( stats.kurtosis(means), 3)
 
-	return means, std_dev
+
+	return means, mean_of_means, std_dev, skewness, kurtosis
 
 
 """
@@ -85,12 +94,19 @@ def find_stats(df):
 	- parameter:	 	pandas Series
 	- return:			none
 """
-def display_stats(means, std_dev):
+def display_stats(means, mean_of_means, std_dev, skewness, kurtosis):
 	for i in range(0, len(means)):
-		print('\tsample number', i+1, '\tmean', means[i])
+		print('\tsample number', i+1, '\tmean\t', means[i])
 
 	print('\n')
-	print('\t\t\t\tstd. deviation', std_dev)
+	# print('\t\t\t\t|–––––––––––––––––––––––––––––––|')
+	print('\t\t\t|–––––––––––––––––––––––|')
+	print('\t\t\t  mean of means ', mean_of_means)
+	print('\t\t\t  std. deviation', std_dev)
+	print('\t\t\t  skewness\t', skewness)
+	print('\t\t\t  kurtosis\t', kurtosis)
+	# print('\t\t\t\t|\t\t\t\t|')
+	print('\t\t\t|–––––––––––––––––––––––|')
 	print('\n\n')
 
 
@@ -102,28 +118,28 @@ def display_stats(means, std_dev):
 """
 def test():
 	run_1 = prompt_user()
-	means_1, std_dev_1 = find_stats(run_1)
-	display_stats(means_1, std_dev_1)
+	means_1, mean_of_means_1, std_dev_1, skew_1, kurt_1 = find_stats(run_1)
+	display_stats(means_1, mean_of_means_1, std_dev_1, skew_1, kurt_1)
 
 	run_2 = prompt_user()
-	means_2, std_dev_2 = find_stats(run_2)
-	display_stats(means_2, std_dev_2)
+	means_2, mean_of_means_2, std_dev_2, skew_2, kurt_2 = find_stats(run_2)
+	display_stats(means_2, mean_of_means_2, std_dev_2, skew_2, kurt_2)
 
 	run_3 = prompt_user()
-	means_3, std_dev_3 = find_stats(run_3)
-	display_stats(means_3, std_dev_3)
+	means_3, mean_of_means_3, std_dev_3, skew_3, kurt_3 = find_stats(run_3)
+	display_stats(means_3, mean_of_means_3, std_dev_3, skew_3, kurt_3)
 
 	run_4 = prompt_user()
-	means_4, std_dev_4 = find_stats(run_4)
-	display_stats(means_4, std_dev_4)
+	means_4, mean_of_means_4, std_dev_4, skew_4, kurt_4 = find_stats(run_4)
+	display_stats(means_4, mean_of_means_4, std_dev_4, skew_4, kurt_4)
 
 	run_5 = prompt_user()
-	means_5, std_dev_5 = find_stats(run_5)
-	display_stats(means_5, std_dev_5)
+	means_5, mean_of_means_5, std_dev_5, skew_5, kurt_5 = find_stats(run_5)
+	display_stats(means_5, mean_of_means_5, std_dev_5, skew_5, kurt_5)
 
 	run_6 = prompt_user()
-	means_6, std_dev_6 = find_stats(run_6)
-	display_stats(means_6, std_dev_6)
+	means_6, mean_of_means_6, std_dev_6, skew_6, kurt_6 = find_stats(run_6)
+	display_stats(means_6, mean_of_means_6, std_dev_6, skew_6, kurt_6)
 
 test()
 
