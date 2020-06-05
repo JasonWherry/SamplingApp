6f21c100e00daffcb811a91ed70b00f3e5498d96
Jason Wherry <jw473415@ohio.edu>
2020-06-05 19:02:28 -0400

    Updated User Menu & Extended Dist. Types

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Author: Jason Wherry	Start Date: 5/08/2020		To Run: python3 main.py
 What am I actually plotting? --> The means calculated from each sample
 
 Next steps:
-	- Create a Menu for the user
+	- Add on to educate()
 """
 
 import numpy as np
@@ -19,27 +19,52 @@ import scipy.stats as stats
 """
  prompt_user()
  	- objective:		gather data aligned with user's request
-	- parameter:	 	none
+	- parameter:	 	None
 	- return:			DataFrame – filled with values randomly generated between 0 & 1
 """
-def prompt_user():
-	population = 3200
+def prompt_user(distribution):
+	population = input('\nEnter \'q\' to quit\nChoose a Population size - 3200  6400  9600, default=3200: ') #3200
+	population_sizes = ['3200', '6400', '9600']
+	
+	# check input to stop code execution
+	if population == 'q' or population == 'Q':
+		exit()
+	if population == '':
+		population = '3200'
+
+	while population not in population_sizes:
+		print(population, ' - invalid population')
+		population = input('\nEnter \'q\' to quit\nChoose a Population size - 3200  6400  9600, default=3200: ')
+		
+		# check input to stop code execution
+		if population == 'q' or population == 'Q':
+			exit()
+		if population == '':
+			population = '3200'
+
+	print('\npopulation =', population)
+
 	case_sizes = ['1', '40', '80', '160', '320', '640']
-	cases = input('Enter \'q\' to quit\nChoose the number of sample(s) – 1  40  80  160  320  640: ')
+	cases = input('\n\tEnter \'q\' to quit\n\n\tChoose the number of cases – 1  40  80  160  320  640: ')
 
 	# check input to stop code execution
 	if cases == 'q' or cases == 'Q':
 		exit()
 
 	while cases not in case_sizes:
-		print(cases, 'is an invalid option')
-		cases = input('\nEnter \'q\' to quit\nChoose the number of sample(s) – 1  40  80  160  320  640: ')
+		print(cases, ' - invalid case')
+		cases = input('\nEnter \'q\' to quit\nChoose the number of cases – 1  40  80  160  320  640: ')
 		
 		# check input to stop code execution
 		if cases == 'q' or cases == 'Q':
 			exit()
 
-	samples = int(population / int(cases) )
+	print('cases =', cases)
+
+	population_int = int(population)
+
+	samples = int(population_int / int(cases) )
+	print('samples =', str(samples) )
 	print('\n')
 
 	# empty data frame to hold the data; each column is a case
@@ -47,28 +72,51 @@ def prompt_user():
 
 	# fill the DataFrame with data;		df.shape = [samples X cases]
 	# each column is a sample case with the number of values equal to the ratio (population/cases)
-	for i in range(0, int(cases) ):
-		temp = pd.Series(np.random.uniform(0, 1.0, samples))
-		col_name = 'Case ' + str(i+1)
-		df.insert(i, col_name, temp)
+
+	if distribution == 'Uniform':
+		for i in range(0, int(cases) ):
+			temp = pd.Series(np.random.uniform(0, 1.0, samples))
+			col_name = 'Case ' + str(i+1)
+			df.insert(i, col_name, temp)
+
+	# elif distribution == 'Bernoulli':
+		# for i in range(0, int(cases) ):
+		# 	temp = pd.Series(np.random.binomial(1, 0.5, samples))
+		# 	col_name = 'Case ' + str(i+1)
+		# 	df.insert(i, col_name, temp)
+		# # Not working at the moment
+
+	elif distribution == 'Binomial':
+		for i in range(0, int(cases) ):
+			temp = pd.Series(np.random.binomial(1, 0.5, samples)) # flip a coin 1 time, tested samples times
+			col_name = 'Case ' + str(i+1)
+			df.insert(i, col_name, temp)
+
+	elif distribution == 'Normal':
+		for i in range(0, int(cases) ):
+			temp = pd.Series(np.random.normal(0.5, 0.1, samples))
+			col_name = 'Case ' + str(i+1)
+			df.insert(i, col_name, temp)
 
 	print(df, '\n') # display np.random.uniform numbers (dtype is float64) by sample
+	# print(df.shape)
 
-	return df
+	return df, distribution
 
 
 """
  find_mean(data_frame)
  	- objective:		determine the mean of the chosen sample(s)
-	- parameter:	 	pandas DataFrame
+	- parameter:	 	pandas DataFrame, type of distribution
 	- return:			Series – means & std. deviations
 """
-def find_stats(df):
+def find_stats(df, dist_type):
 	means = []
-	sample_num = len(df.columns)
-	sample_size = len(df)
+	sample_num = len(df.columns) 	# AKA cases
+	sample_size = len(df)			# AKA samples per case
+	dist_type = dist_type + ' Distribution - ' + str(sample_num) + ' Samples that observe ' + str(sample_size) + ' numbers'
 
-	print('number of sample(s):\t', sample_num, '\nsize of each sample:\t', sample_size)
+	print('number of cases:\t', sample_num, '\nsamples per case:\t', sample_size)
 	print('\n')
 	
 	# loop through each sample and find its respective mean
@@ -90,8 +138,9 @@ def find_stats(df):
 
 		fig = plt.subplot()
 		fig.hist(numpy_means, bins=50, range=[0,1], histtype='bar')
-		fig.set_xlabel('Mean')
-		fig.set_ylabel('Frequency')
+		fig.set_xlabel('Mean (Value)')
+		fig.set_ylabel('Value Frequency')
+		fig.set_title(dist_type)
 		plt.show()
 
 
@@ -101,8 +150,9 @@ def find_stats(df):
 
 		fig = plt.subplot()
 		fig.hist(numpy_means, bins=50, range=[0,1], histtype='bar')
-		fig.set_xlabel('Mean')
-		fig.set_ylabel('Frequency')
+		fig.set_xlabel('Mean (Value)')
+		fig.set_ylabel('Value Frequency')
+		fig.set_title(dist_type)
 		plt.show()
 
 		mean_of_means = round( numpy_means.mean(), 3)
@@ -118,7 +168,7 @@ def find_stats(df):
  find_mean(data_frame)
  	- objective:		display content of lists
 	- parameter:	 	pandas Series
-	- return:			none
+	- return:			None
 """
 def display_stats(means, mean_of_means, std_dev, skewness, kurtosis):
 	# for i in range(0, len(means)):
@@ -139,19 +189,87 @@ def display_stats(means, mean_of_means, std_dev, skewness, kurtosis):
 """
  test()
  	- objective:		runs the program functions
-	- parameter:	 	none
-	- return:			none
+	- parameter:	 	number of runs desired, selected distribution
+	- return:			None
 """
-def test():
-	run = prompt_user() # returns a data frame
-	means, mean_of_means, std_dev, skew, kurt = find_stats(run)
-	display_stats(means, mean_of_means, std_dev, skew, kurt)
+def test(num_runs, dist):
+	while num_runs > 0:
+		run, dist = prompt_user(dist) # returns a data frame
+		means, mean_of_means, std_dev, skew, kurt = find_stats(run, dist)
+		display_stats(means, mean_of_means, std_dev, skew, kurt)
+
+		num_runs -= 1
 
 
-test() # run 1
-test() # run 2
-test() # run 3
-test() # run 4
-test() # run 5
-test() # run 6
+# test(1, 'Normal') # run once
+
+"""
+educate()
+	- objective:		Provide user with a list of options to run the program
+	- parameter:		None
+	- return:			User's request
+"""
+def educate():
+	print('A Uniform distribution...')
+
+	# print('A Bernoulli distribution...')
+
+	print('A Normal distribution...')
+
+	print('A Binomial distribution...')
+
+
+"""
+ menu()
+	- objective:		Provide user with a list of options to run the program
+	- parameter:		None
+	- return:			User's request
+"""
+def menu():
+	print('\n  Menu')
+	print('\t1 : distributions -> lists alternative distribution choices')
+	print('\t2 : demonstration -> demonstrates the Central Limit Theorem with random sampling')
+	print('\t3 : quit -> terminates program execution')
+
+	user_input = input('\n  Enter one of the following options: \'1\', \'2\', \'3\': ')
+
+	# print(user_input)
+	print('\n')
+
+	# case analysis on user input
+	if user_input == '1':
+		educate()
+		menu() # display menu again
+	elif user_input == '2':
+		print('\n Select a distribution type')
+		print('\t1 : Uniform')
+		# print('\t2 : Bernoulli')
+		print('\t2 : Binomial')
+		print('\t3 : Normal')
+		
+
+		dist_type = input('\n  Enter one of the following options: \'1\', \'2\', \'3\': ')
+		
+
+
+		if dist_type == '1':
+			test(1, 'Uniform')
+		# elif dist_type == '2':
+		# 	test(1, 'Bernoulli')
+		elif dist_type == '2':	
+			test(1, 'Binomial')
+		elif dist_type == '3':
+			test(1, 'Normal')
+		
+
+		menu() # display menu again
+	elif user_input == '3':
+		exit()
+	
+
+menu()
+
+
+
+
 
