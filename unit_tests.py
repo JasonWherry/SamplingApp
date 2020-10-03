338ab48d77f1fd1eee07b275182327b76fa4a6e1
Jason Wherry <jw473415@ohio.edu>
2020-10-03 13:14:10 -0400

    deleted test_Menu() in 'unit_tests.py'. took requirements.txt out of '.travis.yml'

--- unit_tests.py
+++ unit_tests.py
@@ -5,10 +5,5 @@ class TestFunctions(unittest.TestCase):
 	def test1(self):
 		self.assertEqual(2/2, 1)
 
-	# def test_Menu(self):
-	# 	ret = menu()
-	# 	print('ret:  ', ret)
-	# 	# self.assertNotEqual(ret,'Jay')
-
 if __name__ == '__main__':
     unittest.main()
